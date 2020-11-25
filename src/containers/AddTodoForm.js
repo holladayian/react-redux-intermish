236f07f0afbd49d40aa954baac6d9342cff4b799
Ian Holladay <holladayian@gmail.com>
2020-11-25 14:03:17 -0900

    Display ideas after submitted

--- src/containers/AddTodoForm.js
+++ src/containers/AddTodoForm.js
@@ -1,41 +1,6 @@
-/*
-import React, { Component } from 'react'
-
-class AddTodoForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { todo: '' };
-  }
-  
-  handleChange = (e) => {
-    this.setState({ todo: e.target.value });
-  }
-  
-  submitForm = (e) => {
-    e.preventDefault()
-    this.setState({ todo: '' });
-  }
-  
-  render() {
-    return (
-      <section>
-      <form onSubmit={this.submitForm}>
-      <input
-      value={this.state.todo}
-      placeholder="Add A Todo"
-      onChange={this.handleChange} />
-      <button>Add Todo</button>
-      </form>
-      </section>
-      )
-    }
-  }
-  export default AddTodoForm;
- */
-
-import React, { Component } from 'react'
-import { addToDo } from '../actions';
-import { connect } from 'react-redux';
+ import React, { Component } from 'react'
+ import { addToDo } from '../actions';
+ import { connect } from 'react-redux';
 
 class AddTodoForm extends Component {
   constructor(props) {
