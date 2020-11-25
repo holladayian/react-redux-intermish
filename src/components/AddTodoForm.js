3fcd20beb0d72ba0cc98488c9473fc7058902ad9
Ian Holladay <holladayian@gmail.com>
2020-11-24 23:36:18 -0900

    mapDispatchToProps

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,3 +1,4 @@
+/*
 import React, { Component } from 'react'
 
 class AddTodoForm extends Component {
@@ -5,14 +6,51 @@ class AddTodoForm extends Component {
     super(props);
     this.state = { todo: '' };
   }
-
+  
   handleChange = (e) => {
     this.setState({ todo: e.target.value });
+  }
+  
+  submitForm = (e) => {
+    e.preventDefault()
+    this.setState({ todo: '' });
+  }
+  
+  render() {
+    return (
+      <section>
+      <form onSubmit={this.submitForm}>
+      <input
+      value={this.state.todo}
+      placeholder="Add A Todo"
+      onChange={this.handleChange} />
+      <button>Add Todo</button>
+      </form>
+      </section>
+      )
+    }
+  }
+  export default AddTodoForm;
+ */
+
+import React, { Component } from 'react'
+import { addToDo } from '../actions';
+import { connect } from 'react-redux';
+
+class AddTodoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { todo: '' };
   }
 
   submitForm = (e) => {
     e.preventDefault()
+    this.props.addToDo(this.state.todo)
     this.setState({ todo: '' });
+  }
+
+  handleChange = (e) => {
+    this.setState({ todo: e.target.value });
   }
 
   render() {
@@ -30,4 +68,9 @@ class AddTodoForm extends Component {
   }
 }
 
-export default AddTodoForm;
+const mapDispatchToProps = dispatch => ({
+  addToDo: text => dispatch( addToDo(text) )
+})
+
+export default connect(null, mapDispatchToProps)(AddTodoForm);
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './components/App';
-// import './index.css';
-
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
