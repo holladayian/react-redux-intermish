55467cf8a75d470a5b22611252ba3a93db466459
Ian Holladay <holladayian@gmail.com>
2020-11-25 15:15:19 -0900

    ToDo toggleToDo and mapDispatchToProps

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { toggleToDo } from '../../actions';
 
-const ToDo = ({ id, todo, completed }) => {
-  return (
-    <li>{todo}</li>
-  )
+const ToDo = ({ id, todo, completed, toggleToDo }) => {
+    return(
+        <li 
+            className={completed ? 'completed' : 'incomplete'}
+            onClick={() => toggleToDo(id)}
+        >
+            {todo}
+        </li>
+    )
 }
 
-export default ToDo;
+const mapDispatchToProps = dispatch => ({
+    toggleToDo: id => dispatch( toggleToDo(id) )
+})
+
+export default connect(null, mapDispatchToProps)(ToDo);
